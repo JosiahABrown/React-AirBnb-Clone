2b4a5663a5cad364c6fde8eb91886922224ab8c8
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-03-11 15:20:04 -0600

    Basic Card component layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HeroComponent from './components/Hero';
+import Card from './components/Card';
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <HeroComponent />
+      <Card />
     </div>
   )
 }
