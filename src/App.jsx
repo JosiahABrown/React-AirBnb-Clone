ee4aa962e558eafdd40f71ad4f846aba0806de9d
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2023-03-16 20:05:04 -0500

    horizontal cards

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ export default function App() {
     <div>
       <Navbar />
       <HeroComponent />
-      {cards}
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,11 +8,11 @@ export default function Card(props) {
             <div className="card-stats">
                 <img className="card-star" src={star} alt="star" />
                 <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount})</span>
-                <span className="gray">{props.location}</span>
+                <span className="gray">({props.reviewCount})  • </span>
+                <span className="gray"> {props.location}</span>
             </div>                
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card-title">{props.title}</p>
+            <p className="card-price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
